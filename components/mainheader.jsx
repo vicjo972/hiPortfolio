549a0699ac8e150cf76754481df6332b2b145b0a
vicjo972 <vicjo972@student.liu.se>
2022-12-04 04:19:35 +0100

    tried to fix bug

--- components/mainheader.jsx
+++ components/mainheader.jsx
@@ -2,7 +2,7 @@ export default function MainHeader() {
     return (
         <div class="flex items-center justify-center h-5/6">
             <h1 class="text-4xl sm:text-6xl lg:text-8xl font-bold">
-                hi, i'm victor
+                hi, i&apos;m victor
             </h1>
         </div>
     )
