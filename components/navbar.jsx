d4d9f7b6bb430d90ea2798404d70fc4c1da5fdde
vicjo972 <vicjo972@student.liu.se>
2022-12-04 04:48:48 +0100

    went back to a-tag

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,12 +1,10 @@
-import Link from "next/link"
-
 export default function Navbar() {
   return (
     <nav class="top-0 sticky flex justify-center py-10 px-6 bg-gradient-to-b from-portfolioBlue-50 z-50">
       <div>
-        <Link href="#about" class=" sm:text-xl font-bold hover:text-red-900 m-5">//about</Link>
-        <Link href="#projects" class="lg:text-xl font-bold no-underline hover:text-blue-500 m-5">//projects</Link>
-        <Link href="#contact" class="lg:text-xl font-bold no-underline hover:text-green-700 m-5">//contact</Link>
+        <a href="#about" class=" sm:text-xl font-bold hover:text-red-900 m-5">{'//'}about</a>
+        <a href="#projects" class="lg:text-xl font-bold no-underline hover:text-blue-500 m-5">{'//'}projects</a>
+        <a href="#contact" class="lg:text-xl font-bold no-underline hover:text-green-700 m-5">{'//'}contact</a>
       </div>
     </nav>
   )
