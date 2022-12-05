d45909ee1079e33333722b53eec03033f1bbb3e1
vicjo972 <vicjo972@student.liu.se>
2022-12-05 01:35:23 +0100

    fixed error

--- components/mainheader.jsx
+++ components/mainheader.jsx
@@ -4,7 +4,7 @@ export default function MainHeader() {
     return (
         <div class="flex items-center justify-center h-5/6">
             <h1 class="text-4xl sm:text-6xl lg:text-8xl font-bold">
-                Hi, I'm Victor.
+                Hi, I&apos;m Victor.
             <Typewriter
                 options={{
                     strings: ['Software Dev', 'Data Science', 'Tech Nerd😎'],
